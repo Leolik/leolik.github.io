6114a6c79769f9432e90a1d4fabbc4f96d0a9585
Roman Sukochev <rsukochev@yandex.ru>
2018-10-29 18:33:36 +0300

    Add pwa (test)

---
+++ rpsc-crimea-vk/sw.js
@@ -0,0 +1,4 @@
+importScripts('sw-toolbox.js');
+toolbox.precache(["/index.html","/style.css","/js/*.js","/*.png","/*.svg","/favicon.ico"]);
+toolbox.router.get('*', toolbox.networkFirst, {
+networkTimeoutSeconds: 3});
