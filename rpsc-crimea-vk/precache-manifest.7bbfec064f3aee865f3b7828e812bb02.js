303b4a8aeb192bb4f4b5eb82732b2f6a6fb7a546
Roman Sukochev <rsukochev@yandex.ru>
2018-11-02 23:17:44 +0300

    Update pwa app

---
+++ rpsc-crimea-vk/precache-manifest.7bbfec064f3aee865f3b7828e812bb02.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "https://sukochev.name/rpsc-crimea-vk/robots.txt"
+  },
+  {
+    "revision": "a9386bd12f44029bc7c0",
+    "url": "https://sukochev.name/rpsc-crimea-vk/js/chunk-vendors.1c4441a9.js"
+  },
+  {
+    "revision": "ddc30a804351931c2e21",
+    "url": "https://sukochev.name/rpsc-crimea-vk/js/app.015da520.js"
+  },
+  {
+    "revision": "e8b0e182a40f86c0147fc76ac7d4368c",
+    "url": "https://sukochev.name/rpsc-crimea-vk/index.html"
+  },
+  {
+    "revision": "c48a7137e6e4e9cf3da57dbb3935a5d8",
+    "url": "https://sukochev.name/rpsc-crimea-vk/fonts/triodion-ieUcs.c48a7137.woff"
+  },
+  {
+    "revision": "54ca447d49333270a885866c374b8048",
+    "url": "https://sukochev.name/rpsc-crimea-vk/fonts/triodion-ieUcs.54ca447d.woff2"
+  },
+  {
+    "revision": "a9386bd12f44029bc7c0",
+    "url": "https://sukochev.name/rpsc-crimea-vk/css/chunk-vendors.09ab00d2.css"
+  },
+  {
+    "revision": "ddc30a804351931c2e21",
+    "url": "https://sukochev.name/rpsc-crimea-vk/css/app.2a6de36c.css"
+  },
+  {
+    "revision": "c3d1aaa7d42e7321c55626049637f92f",
+    "url": "https://sukochev.name/rpsc-crimea-vk/browserconfig.xml"
+  }
+];
