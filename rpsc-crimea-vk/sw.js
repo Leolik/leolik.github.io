b78d1164057d88d60b57b0f80b76f41b64664983
Roman Sukochev <rsukochev@yandex.ru>
2018-11-02 18:30:23 +0300

    Add pwa (test) https changes

--- rpsc-crimea-vk/sw.js
+++ rpsc-crimea-vk/sw.js
@@ -1,4 +1,4 @@
-importScripts('sw-toolbox.js');
-toolbox.precache(["/index.html","/style.css","/js/*.js","/*.png","/*.svg","/favicon.ico"]);
-toolbox.router.get('*', toolbox.networkFirst, {
-networkTimeoutSeconds: 3});
+
+importScripts('https://sukochev.name/rpsc-crimea-vk/sw-toolbox.js');
+toolbox.precache(['/index.html','/style.css']);
+toolbox.router.get('*', toolbox.networkFirst, {networkTimeoutSeconds: 3});
